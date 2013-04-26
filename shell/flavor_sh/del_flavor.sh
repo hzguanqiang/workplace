2608140af8f402950e462aef3c37eff7d6bdef16
Jackie <hzguanqiang@gmail.com>
2013-04-26 20:15:05 +0800

    refresh

--- shell/flavor_sh/del_flavor.sh
+++ shell/flavor_sh/del_flavor.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-flavor_id=11
-while [ "$flavor_id" -lt "911" ]
+flavor_id=`nova flavor-list | grep ecus| grep -v flavor_ | awk '{print $2}' | sort | tail -n1`
+flavor_id=`expr $flavor_id + 1`
+max_flavor_id=`expr $flavor_id + 722`
+while [ "$flavor_id" -lt "$max_flavor_id" ]
 do
     echo nova flavor-delete $flavor_id
     nova flavor-delete $flavor_id
