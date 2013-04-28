e1b6f5aa1f1afcfe63366c8413abe6e42716a231
Jackie <hzguanqiang@gmail.com>
2013-04-28 10:42:26 +0800

    os-quotas-usage

--- ops_test/os-quotas-usage.sh
+++ ops_test/os-quotas-usage.sh
@@ -1,1 +1,6 @@
-http get localhost:8774/v2/49d0548301a440f2bb5374b8ceef3a73/os-quotas-usage/hosts x-auth-token:fd927658201d46229ff8240d9ff13200
+#!/bin/bash
+
+tenant_id=`keystone token-get | grep tenant_id | awk '{print $4}'`
+token=`keystone token-get | grep id | grep -v tenant_id | grep -v user_id| awk '{print $4}'`
+echo http get localhost:8774/v2/$tenant_id/os-quotas-usage/hosts x-auth-token:$token
+http get localhost:8774/v2/$tenant_id/os-quotas-usage/hosts x-auth-token:$token
