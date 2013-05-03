3a0d4066d232439e100f03209eeb6700c854101c
Jackie <hzguanqiang@gmail.com>
2013-05-03 15:24:27 +0800

    ..

---
+++ ioqos_test/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+./randwrite.sh
+sleep 3
+./randwrite.sh
+sleep 3
+./randwrite.sh
+sleep 3
+./randread.sh
+sleep 3
+./randread.sh
+sleep 3
+./randread.sh
+sleep 3
+./write.sh
+sleep 3
+./write.sh
+sleep 3
+./write.sh
+sleep 3
+./read.sh
+sleep 3
+./read.sh
+sleep 3
+./read.sh
