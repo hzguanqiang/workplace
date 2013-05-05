08b1749489936efa5f2546aadd7414cc26fbd215
Jackie <hzguanqiang@gmail.com>
2013-05-05 13:19:07 +0800

    ..

---
+++ ops_test/az_statics.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+tenant_id=`keystone token-get | grep tenant_id | awk '{print $4}'`
+token=`keystone token-get | grep id | grep -v tenant_id | grep -v user_id| awk '{print $4}'`
+echo http get localhost:9800/$tenant_id/statistics/az x-auth-token:$token
+http get localhost:9800/$tenant_id/statistics/az x-auth-token:$token
