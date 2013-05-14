95d4b61b10fccd54032f5460506f23dbefec5b80
Jackie <hzguanqiang@gmail.com>
2013-05-14 12:48:52 +0800

    flavor_list

--- ops_test/az_statics.sh
+++ ops_test/az_statics.sh
@@ -2,5 +2,5 @@
 
 tenant_id=`keystone token-get | grep tenant_id | awk '{print $4}'`
 token=`keystone token-get | grep id | grep -v tenant_id | grep -v user_id| awk '{print $4}'`
-echo http get localhost:9800/$tenant_id/statistics/az/nova5 x-auth-token:$token
-http get localhost:9800/$tenant_id/statistics/az/nova5 x-auth-token:$token
+echo http get localhost:9800/$tenant_id/statistics/az/nova x-auth-token:$token
+http get localhost:9800/$tenant_id/statistics/az/nova x-auth-token:$token
