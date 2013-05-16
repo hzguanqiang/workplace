93f4f64415fd4c53cb80cbcdc2665fc9bcf662b2
Jackie <hzguanqiang@gmail.com>
2013-05-16 13:14:10 +0800

    ..

--- flavor_sh/output_flavor_for_frontend.sh
+++ flavor_sh/output_flavor_for_frontend.sh
@@ -6,7 +6,7 @@
 # file 'all_flavor_for_frontend.csv'
 
 echo name,id,memory_mb,vcpus,swap,vcpu_weight,flavorid,rxtx_factor,root_gb,ephemeral_gb,disable,is_public,ecus_per_cpu > temp.csv
-nova-manage flavor list | sed "s/:\|,\|MB\|GB\|Gb\|u'\|'}//g" |awk '{print $1",,"$3","$5","$13",null,"$11","$16","$7","$9",0,"$17","$20}' >> temp.csv
+sudo nova-manage flavor list | sed "s/:\|,\|MB\|GB\|Gb\|u'\|'}//g" |awk '{print $1",,"$3","$5","$13",null,"$11","$16","$7","$9",0,"$17","$20}' >> temp.csv
 
 sort -t"," -n -k7 temp.csv > all_flavor_for_frontend.csv
 
