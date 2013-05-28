1d32a7c33cd560b6570eac2e004c53fffa48dee3
Jackie <hzguanqiang@gmail.com>
2013-05-28 14:32:29 +0800

    add 32core 50% pressure for cpuqos test

---
+++ cpuqos_test_new/32core_50%/test_8.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+port=22
+
+name=test_8
+
+nohup ./start_test.sh 8 &
+nohup ./cpu_rate.sh $name &
+
+nohup ./start_pressure.sh 32 4 &
+nohup ./start_pressure.sh 24 4 &
+nohup ./start_pressure.sh 16 2 &
+nohup ./start_pressure.sh 12 2 &
+nohup ./start_pressure.sh 6 1 &
+nohup ./start_pressure.sh 4 2 &
+nohup ./start_pressure.sh 2 1 &
+nohup ./start_pressure.sh 1 1 &
+
+while (true)
+do
+  result=`./check_finish.sh 8`
+  echo $result
+
+  ok=`echo $result| grep "runspec finished at"`
+  if [ "$ok"s != ""s ]
+  then
+    echo $result >> ${name}.output
+    echo finished
+    ./stop.sh
+    break
+  else
+    echo not finished
+  fi
+done
