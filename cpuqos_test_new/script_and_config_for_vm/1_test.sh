94972b92e54716d59ce9ae899eee7ae9aea7bab2
Jackie <hzguanqiang@gmail.com>
2013-05-30 16:40:09 +0800

    script for vm

--- cpuqos_test_new/script_and_config_for_vm/1_test.sh
+++ cpuqos_test_new/script_and_config_for_vm/1_test.sh
@@ -11,5 +11,5 @@ source shrc
 pkill runspec
 pkill runspec
 pkill runspec
-echo runspec --rate=${cfg} --iterations=1 --config=${cfg}.cfg 401 403 429 464 473 483 482
-nohup runspec --rate=${cfg} --iterations=1 --config=${cfg}.cfg 401 403 429 464 473 483 482 &
+echo runspec --rate=${cfg} --action clean --iterations=1 --config=${cfg}.cfg 401 403 429 464 473 483 482
+nohup runspec --rate=${cfg} --action clean --iterations=1 --config=${cfg}.cfg 401 403 429 464 473 483 482 &
