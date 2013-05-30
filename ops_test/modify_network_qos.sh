7cc862ff08e9cfbf41ee00efaa96921d7e369e0d
Jackie <hzguanqiang@gmail.com>
2013-05-30 15:35:24 +0800

    modify_network_qos.sh

--- ops_test/modify_network_qos.sh
+++ ops_test/modify_network_qos.sh
@@ -3,5 +3,5 @@ set -x
 tenant_id=`keystone token-get | grep tenant_id | awk '{print $4}'`
 token=`keystone token-get | grep id | grep -v tenant_id | grep -v user_id| awk '{print $4}'`
 
-#curl 127.0.0.1:8774/v2/$tenant_id/servers/948f7c2d-54af-4979-9ca6-52f7443c8493/network-qos/private -X PUT -H "X-Auth-Token:$token" -H "Content-Type: application/json" -d "{private_rate:200}" | python -m json.tool
-curl 127.0.0.1:8774/v2/$tenant_id/servers/948f7c2d-54af-4979-9ca6-52f7443c8493/network-qos/all -X PUT -H "X-Auth-Token:$token" -H "Content-Type: application/json" -d "{private_rate:200,public_rate:200}" -v
+#curl localhost:8774/v2/$tenant_id/servers/1c7ee524-2c48-4d4a-93e0-58203b02ca30/network-qos/private -X PUT -H "X-Auth-Token:$token" -H "Content-Type: application/json" -d "{private_rate:2}" | python -m json.tool
+curl localhost:8774/v2/$tenant_id/servers/1c7ee524-2c48-4d4a-93e0-58203b02ca30/network-qos/public -X PUT -H "X-Auth-Token:$token" -H "Content-Type: application/json" -d '{"rate":2}' -v
