52a8ca1912c763a67657478224c509ed3ea25a2d
Jackie <hzguanqiang@gmail.com>
2014-07-01 16:56:56 +0800

    modify health check script

--- tools/health_check.sh
+++ tools/health_check.sh
@@ -8,7 +8,7 @@ if [ $mem -lt 102400 ]
 then
     zenity --error --text='Out of Memory, mem: '$mem --title=’Warning‘
 else
-    echo 'Memory has '$mem 'remaining, more than 100MB'
+    echo 'Memory has '$mem 'remaining, more than 100MB' > /dev/null
 fi
 
 sleep 3
@@ -17,9 +17,13 @@ load=`cat /proc/loadavg | awk '{print $2}'`
 echo $load
 if [ $(echo "$load >= 2"|bc) = 1 ]
 then
-    zenity --error --text='load more than 2' --title='Warning'
+    # kill the program which has the highest cpu usage
+    pid=`top -b -n1 | sed -n 8p | awk '{print $1}'`
+    process_name=`top -b -n1 | sed -n 8p | awk '{print $12}'`
+    sudo kill -9 $pid
+    zenity --error --text='load more than 2, kill process '$process_name --title='Warning'
 else
-    echo 'load: '$load
+    echo 'load: '$load ', little than 2' > /dev/null
 fi
 
 sleep 30
