7fb0c1f15c2261373a791625f6c10c6f99401ffd
Jackie <hzguanqiang@gmail.com>
2014-10-21 15:46:24 +0800

    add some shell scripts

---
+++ shell/interactive_sh/1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash  
+(echo "qwerty"   
+sleep 1  
+echo "qwerty")|passwd
---
+++ shell/interactive_sh/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read -p "enter number:" no
+read -p "enter name:" name
+echo you have entered $no,$name
