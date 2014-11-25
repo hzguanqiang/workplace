546c48a308b2a5a244697a14ad0514a537594459
Jackie <hzguanqiang@gmail.com>
2014-11-25 12:49:14 +0800

    update autojump configure for oh-my-zsh

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,4 +1,5 @@
 sudo apt-get install zsh
+git clone https://github.com/joelthelion/autojump.git
 ./autojump/install.py
 cp -rf ../oh-my-zsh ~/.oh-my-zsh
 cp -rf templates/zshrc.zsh-template ~/.zshrc 
