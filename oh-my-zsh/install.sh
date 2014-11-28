8a5ffe2448cf09250abb47d111ad9c4e7c49011d
Guan Qiang <hzguanqiang@gmail.com>
2014-11-28 11:26:15 +0800

    Refresh oh-my-zsh config

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,5 +1,6 @@
 sudo apt-get install zsh
 git clone https://github.com/joelthelion/autojump.git
-./autojump/install.py
 cp -rf ../oh-my-zsh ~/.oh-my-zsh
 cp -rf templates/zshrc.zsh-template ~/.zshrc 
+cd autojump
+./install.py
