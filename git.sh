45f00a60eda5df076e1a3b47c4eacf2b48f65a26
Guan Qiang <hzguanqiang@gmail.com>
2015-02-13 15:02:44 +0800

    update scripts

--- git.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp .gitconfig ~/
-git remote set-url origin git@github.com:hzguanqiang/scripts.git
--- ssh_key/config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp ssh_config ~/.ssh/config
-
-cp -rf new_id_rsa ~/.ssh/id_rsa
-cp -rf new_id_rsa.pub ~/.ssh/id_rsa.pub
-chmod 600 ~/.ssh/id_rsa*
